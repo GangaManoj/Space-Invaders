3f86eebc79146905bf5fd9861dee4a27d9c3e4f6
Ganga Manoj <ganga.manoj98@gmail.com>
2020-10-04 19:51:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+
+#initialise pygame
+pygame.init()
+
+#creating the screen
+screen = pygame.display.set_mode((800, 600))
+
+#setting the title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+#player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+dx = 0
+
+def player(x,y):
+    screen.blit(playerImg, (x,y))
+
+#game loop
+running = True
+while running:
+    #changing the display color
+    screen.fill((0,0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:  #checks if any key was pressed
+            if event.key == pygame.K_LEFT:  #checks if the key pressed was the left arrow key
+                dx = -0.1
+            if event.key == pygame.K_RIGHT:  #checks if the key pressed was the right arrow key
+                dx = 0.1
+
+        if event.type == pygame.KEYUP:  #checks if key was just released
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                dx = 0
+
+    playerX += dx
+    player(playerX, playerY)
+    pygame.display.update() #mandatory line - updates the display continuously
+
